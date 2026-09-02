99c31a91bef9bea5ed141811571c9b1dab2d5404
audio123 <audio@123.com>
2026-09-02 23:54:47 +0800

    fix(list): BatchAudioParamsDialog补漏的Modifier import

--- app/src/main/java/com/github/jing332/tts_server_android/compose/systts/list/BatchAudioParamsDialog.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/compose/systts/list/BatchAudioParamsDialog.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.github.jing332.compose.widgets.AppDialog
 import com.github.jing332.compose.widgets.AppSpinner
