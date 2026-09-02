4dbabac6e5055ff74d2edc7c90fc968558abcb4a
audio123 <audio@123.com>
2026-09-02 18:14:18 +0800

    fix(audio): restore divider import

--- app/src/main/java/com/github/jing332/tts_server_android/compose/systts/plugin/PluginAudioParamsDialog.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/compose/systts/plugin/PluginAudioParamsDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
