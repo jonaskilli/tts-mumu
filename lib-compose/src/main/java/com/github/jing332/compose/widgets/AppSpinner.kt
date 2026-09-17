45b41297096e59c6c497fc9765322ea9adfbab0f
audio123 <audio@123.com>
2026-09-17 21:07:11 +0800

    修 CI：AppSpinner 误留两行重复的 RowScope import

--- lib-compose/src/main/java/com/github/jing332/compose/widgets/AppSpinner.kt
+++ lib-compose/src/main/java/com/github/jing332/compose/widgets/AppSpinner.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
